50c28e03005a6139fbe79601fe1bd4613e0fc192
Fredson Costa <fredsoncostaa@gmail.com>
2022-04-30 11:41:44 -0300

    Add tailwind css in project

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className={'flex jus'}>
+        <h1 className="text-yellow-500">Testando</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./src/pages/**/*.{html,js,jsx,tsx}', 
+            './src/components/**/*.{html,js,tsx,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
