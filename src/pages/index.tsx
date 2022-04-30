e436ef93d759648f716cc63fdf672fbea2820b10
Fredson Costa <fredsoncostaa@gmail.com>
2022-04-30 12:30:22 -0300

    create class client and ajusting the component layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
-
+import Layout from "../components/Layout"
 
 export default function Home() {
   return (
-    <div className={'flex jus'}>
-        <h1 className="text-yellow-500">Testando</h1>
+    <div className={`
+        flex content-center justify-center items-center h-screen 
+        bg-gradient-to-r from-purple-500 to-purple-700
+        text-white
+        `}>
+
+          <Layout titulo="Cadastre Simple">
+
+            <span>Content</span>
+          </Layout>
+          
     </div>
   )
 }
